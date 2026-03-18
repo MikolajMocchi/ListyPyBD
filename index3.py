34049cb6755401afef9fdd9ad043d65c70760d76
Mocchi <mikolajgolonka43@gmail.com>
2026-03-18 15:25:30 +0100

    Add files via upload

---
+++ index3.py
@@ -0,0 +1,18 @@
+list = [1,2,3,4,5,6,7,8,9]
+
+#zad 1
+print(list[0])
+print(list[8])
+
+#zad 2
+list.append(10)
+
+#zad 3
+list.remove(4)
+
+#zad 4
+if 2 in list:
+    print('Tak')
+else:
+    print('Nie')
+    
