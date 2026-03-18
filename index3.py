1917729a16bab45b10823c7a46c2f89b9a310d38
Mocchi <mikolajgolonka43@gmail.com>
2026-03-18 15:40:01 +0100

    Update index3.py

--- index3.py
+++ index3.py
@@ -1,5 +1,11 @@
 list = [1,2,3,4,5,6,7,8,9]
 
+slownik = {
+    'imie' = 'Nigel'
+    'wiek' = '17'
+    'home' = 'place'
+}
+
 #zad 1
 print(list[0])
 print(list[8])
@@ -15,4 +21,8 @@ if 2 in list:
     print('Tak')
 else:
     print('Nie')
-    
+
+#zad 5
+print(list[0:8])
+
+
